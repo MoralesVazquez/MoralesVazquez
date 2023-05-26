043f7b422dc63ed9df481eb239b1367f8debdc5f
MoralesVazquez <132928826+MoralesVazquez@users.noreply.github.com>
2023-05-26 11:17:13 -0600

    Add files via upload

---
+++ depurar_programa_Morales.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+{
+	float c[5];
+	float r=0;
+	int i;
+	
+	for (i=0; i<5; i++){
+		
+		printf("%d\n",i+1);
+		scanf("%f\n",&c[i]);
+		r=r+c[i];
+		printf("\n");
+    } 
+	printf("%.2f\n", r/5);
+	return 0; 
+}
