ba71b1418b6e3af1fdbd6fb38c1c20bcc030cde4
MoralesVazquez <132928826+MoralesVazquez@users.noreply.github.com>
2023-05-26 10:11:18 -0600

    Add files via upload

---
+++ Morales_3.2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char mensaje[20]="atapuerta";
+	/* char mensaje[20];
+	strcpy(mensaje,"Atapuerta\n");*/
+	printf(mensaje);
+	return 0;
+}
---
+++ mi_direc.c
@@ -0,0 +1,6 @@
+/*archivo mi direccion.c*/
+
+printf("Tlaxcala\n");
+printf("Boulevar Mariano Sanchez\n");
+printf("Fracc.xxxxxxx\n");
+printf("Tlaxcala\n");
