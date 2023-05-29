72585d0d3b25853e13d52c7204d15e35e61762ff
MoralesVazquez <132928826+MoralesVazquez@users.noreply.github.com>
2023-05-29 07:59:54 -0600

    Add files via upload

---
+++ tres_valores_Morales.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int a,b,c;
+int main()
+{
+	int valor;
+	printf("Tres valores: ");
+	scanf("%d %d &d\a",&a,&b,&c);
+	valor=a+b+c;
+	printf("Valor de a: %d\n",a);
+	printf("Valor de b: %d\n",b);
+	printf("Valor de c: %d\n",c);
+	printf("Suma de las variables: %d",valor);
+	return 0;
+}
